25be22196ce314d26b537a3d848e53fa77ea1489
Sake <98163611+luna-0204@users.noreply.github.com>
2025-11-18 00:07:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsxts,tsx,html}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.{html,js,jsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
